5a29275252a3f1a354bc1e76bc04ba6dfa08bd26
Josh Mills <josh@trueheart78.com>
2025-08-08 15:12:05 -0400

    oop inheritance

--- oop-01-pet.lua
+++ oop-01-pet.lua
@@ -3,6 +3,7 @@
 -- Objects are capilatized
 local function Pet(name)
   name = name or "Luis" -- nil check/default argument
+
   return {
     name = name,
     status = "hungry",
@@ -27,12 +28,51 @@ local function Pet(name)
   }
 end
 
-local dog = Pet()
-dog:print()
-if dog:is_hungry() then
+local dog1 = Pet()
+dog1:print()
+if dog1:is_hungry() then
   -- instance means it needs a colon cuz it's an instance method
   -- so if you see "(self)", it means it's an instance call (one sign)
-  dog:feed()
-  dog:print()
+  dog1:feed()
+  dog1:print()
 end
-dog:feed()
+dog1:feed()
+
+local function Doggo(name, breed)
+  local dog = Pet(name)
+
+  dog.breed = breed or "Mutt" -- nil check/default value
+  dog.loyalty = 0
+
+  dog.is_loyal = function(self)
+    return self.loyalty >= 10
+  end
+  -- redefining the function from the parent class
+  dog.feed = function(self)
+    print("*" .. self.name .. " is being fed*")
+    -- reference anoth instance-based method using that there colon(:) and not a dot(.)
+    if self:is_hungry() then
+      print(self.name .. ": *munch* *munch* *munch*")
+      self.status = "happy"
+      self.loyalty = self.loyalty + 5
+    else
+      print(self.name .. ": *curious look*")
+    end
+  end
+  dog.bark = function(self)
+    print(self.name .. ": *woof* *woof*")
+  end
+  -- don't forget to return the dog, sir!
+  return dog
+end
+
+local benson = Doggo("Benson", "Morkie")
+benson:print()
+benson:bark()
+benson:feed()
+benson.status = "hungry"
+benson:feed()
+
+if benson:is_loyal() then
+  print(benson.name .. " is loyal 󰏩")
+end
